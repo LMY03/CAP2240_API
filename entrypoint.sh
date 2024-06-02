902ece3074a1bd3dbb309c3eaadcab037d8e009c
LMY03 <86579817+LMY03@users.noreply.github.com>
2024-06-02 23:17:08 +0800

    a

--- entrypoint.sh
+++ entrypoint.sh
@@ -32,7 +32,7 @@
 # Run migrations
 python manage.py makemigrations
 python manage.py migrate
-python manage.py runserver 0.0.0.0:8000
+# python manage.py runserver 0.0.0.0:8000
 
 # Start the Django development server
 # exec "$@"
