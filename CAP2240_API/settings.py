ea0d6f81ed391fccb7e3c4ffd83c2ec8e7d0ccfb
LMY03 <86579817+LMY03@users.noreply.github.com>
2024-06-03 15:02:32 +0800

    Update settings.py

--- CAP2240_API/settings.py
+++ CAP2240_API/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-from decouple import config
+# from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,15 +21,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-sr0*!g82&@md%9rlo39a0s!u=5$y_o&8nvpk+pd+2i(181wkv='
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'django-insecure-sr0*!g82&@md%9rlo39a0s!u=5$y_o&8nvpk+pd+2i(181wkv='
+# SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-DEBUG = config('DEBUG')
+DEBUG = True
+# DEBUG = config('DEBUG')
 
 ALLOWED_HOSTS = ['*']
-ALLOWED_HOSTS = config('ALLOWED_HOSTS')
+# ALLOWED_HOSTS = config('ALLOWED_HOSTS')
 
 
 # Application definition
@@ -88,16 +88,16 @@ DATABASES = {
     }
 }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': config('MYSQL_CAP2240_DATABASE'),
-        'USER': config('MYSQL_CAP2240_USER'),
-        'PASSWORD': config('MYSQL_CAP2240_PASSWORD'),
-        'HOST': config('mysql'),  # Use 'db' as default from .env
-        'PORT': config('DB_PORT'),  # Use '3306' as default from .env
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': config('MYSQL_CAP2240_DATABASE'),
+#         'USER': config('MYSQL_CAP2240_USER'),
+#         'PASSWORD': config('MYSQL_CAP2240_PASSWORD'),
+#         'HOST': config('mysql'),  # Use 'db' as default from .env
+#         'PORT': config('DB_PORT'),  # Use '3306' as default from .env
+#     }
+# }
 
 
 # Password validation
