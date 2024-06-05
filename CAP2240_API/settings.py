e347ec82d60ac6c8e52a3541c8c7c8add8880b0e
LMY03 <86579817+LMY03@users.noreply.github.com>
2024-06-05 22:56:44 +0800

    a

--- CAP2240_API/settings.py
+++ CAP2240_API/settings.py
@@ -36,7 +36,7 @@ ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     "ticketing.apps.TicketingConfig",
-    "proxmoxs.apps.ProxmoxConfig",
+    "proxmox.apps.ProxmoxConfig",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
