e38a8e5e3a2585147b86eaa40e0b98a498ad4168
LMY03 <86579817+LMY03@users.noreply.github.com>
2024-06-06 20:57:51 +0800

    a

--- guacamole/guacamole.py
+++ guacamole/guacamole.py
@@ -1,103 +1,40 @@
 import requests
 import json
 
-# GUACAMOLE_HOST = 'http://192.168.43.147'
-# GUACAMOLE_HOST = 'http://192.168.254.125:8080'
-GUACAMOLE_HOST = 'http://guacamole:8080'
-USERNAME = 'guacadmin'
-PASSWORD = 'guacadmin'
+from .token import get_token
 
-# get token /
-def get_token() :
-    session = requests.Session()
-    # session.verify = CA_CRT
-    response = session.post(
-        f"{GUACAMOLE_HOST}/guacamole/api/tokens",
-        data={ 'username' : USERNAME, 'password' : PASSWORD },
-        # verify=CA_CRT
+GUACAMOLE_HOST = 'http://guacamole:8080/guacamole'
+DATA_SOURCE = 'mysql'
+
+def getHeader(): 
+    token = get_token()
+    headers = { 'Authorization': f'Bearer {token}' }
+    return headers
+
+def create_guacamole_user(username, password):
+    data = {
+        'username': username,
+        'password': password,
+        'attributes': {}
+    }
+    response = requests.post(
+        f"{GUACAMOLE_HOST}/api/session/data/{DATA_SOURCE}/users",
+        headers=getHeader(),
+        json=data
     )
-    data = response.json()
-    return data['authToken']
+    response.raise_for_status()
+    return response.json()
 
-# get session info TODO: avoid request again here
-def get_session_info():
-    session = requests.Session()
-    token = get_token()
-    session.headers.update({ 'Guacamole-Token' : token })
-    url = f"{GUACAMOLE_HOST}/guacamole/api/session/data/mysql/connections"
-    response = session.get(url)
-
-    return response.text
-
-# create user /
-def create_user(username, password):
-    token = get_token()
-    url = f"{GUACAMOLE_HOST}/guacamole/api/session/data/mysql/users?token={token}"
-    config = {
-        "username": username,
-        "password": password,
-        "attributes": {
-            "disabled": "",
-            "expired": "",
-            "access-window-start": "",
-            "access-window-end": "",
-            "valid-from": "",
-            "valid-until": "",
-            "timezone": ""
-        }
+def create_guacamole_connection(name, protocol, parameters):
+    data = {
+        'name': name,
+        'protocol': protocol,
+        'parameters': parameters
     }
-    headers = {'Content-Type': 'application/json'}
-    response = requests.post(url, data=json.dumps(config), headers=headers)
-    return response
-
-# delete user
-def delete_user(username):
-    token = get_token()
-    url = f"{GUACAMOLE_HOST}/guacamole/api/session/data/mysql/users/{username}?token={token}"
-    response = requests.delete(url)
-    return response
-
-# create connection
-def create_connection(name, protocol, port, hostname, username, password, parentIdentifier):
-    token = get_token()
-    url = f"{GUACAMOLE_HOST}/guacamole/api/session/data/mysql/connections?token={token}"
-    config = {
-      "parentIdentifier": parentIdentifier,
-      "name": name,
-      "protocol": protocol,
-      "parameters": {
-        "port": port,
-        "hostname": hostname,
-        "username": username,
-        "password": password
-      },
-      "attributes": {}
-    }
-    headers = {'Content-Type': 'application/json'}
-    response = requests.post(url, data=json.dumps(config), headers=headers)
-
-    # get connection id
-    data = response.json()
-    return data["identifier"]
-
-# delete connection
-def delete_connection(connection_id):
-    token = get_token()
-    url = f"{GUACAMOLE_HOST}/guacamole/api/session/data/mysql/connections/{connection_id}?token={token}"
-    response = requests.delete(url)
-    return response
-
-# assign connection
-def assign_connection(username, connection_id):
-    token = get_token()
-    url = f"{GUACAMOLE_HOST}/guacamole/api/session/data/mysql/users/{username}/permissions?token={token}"
-    config = [
-          {
-            "op": "add",
-            "path": f"/connectionPermissions/{connection_id}",
-            "value": "READ"
-          }
-        ]
-    headers = {'Content-Type': 'application/json'}
-    response = requests.patch(url, data=json.dumps(config), headers=headers)
-    return response
+    response = requests.post(
+        f"{GUACAMOLE_HOST}/api/session/data/{DATA_SOURCE}/connections",
+        headers=getHeader(),
+        json=data
+    )
+    response.raise_for_status()
+    return response.json()
