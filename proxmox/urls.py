221ae40a22f1d186ab795df9c7ee36b2ecffe869
LMY03 <86579817+LMY03@users.noreply.github.com>
2024-06-07 22:18:45 +0800

    a

--- proxmox/urls.py
+++ proxmox/urls.py
@@ -6,7 +6,6 @@ app_name = "proxmox"
 
 urlpatterns = [
     path("", views.renders, name="form"),
-    path("success", views.success, name="success"),
     path("clone_vm", views.clone_vm, name="clone_vm"),
     path("start_vm", views.start_vm, name="start_vm"),
     path("shutdown_vm", views.shutdown_vm, name="shutdown_vm"),
