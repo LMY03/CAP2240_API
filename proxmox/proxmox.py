96dc24d4fc8cc4f823e7d68b093a79fbf88b5791
LMY03 <86579817+LMY03@users.noreply.github.com>
2024-06-08 01:14:51 +0800

    Update proxmox.py

--- proxmox/proxmox.py
+++ proxmox/proxmox.py
@@ -39,7 +39,8 @@ def get_vm_ip(node, vmid, port="ens18"):
     session = get_authenticated_session()
     response = session.get(url)
     # response.raise_for_status()
-    if response.status_code is 500 : get_vm_ip(node, vmid)
+    # if response.status_code is 500 : get_vm_ip(node, vmid)
+    if response.status_code is 500 : return 500
 
     ip_address = None
     for interface in response.json()['data']['result']:
