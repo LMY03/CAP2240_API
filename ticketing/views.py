bd15eb5e53fecb8cb3a61fde67a40ced021d46a0
LMY03 <86579817+LMY03@users.noreply.github.com>
2024-06-08 12:02:26 +0800

    Update views.py

--- ticketing/views.py
+++ ticketing/views.py
@@ -21,7 +21,7 @@ def vm_test(request):
         hostname = views.wait_for_qemu_start(node, new_vm_id) 
 
         guacamole_password = User.objects.make_random_password()
-        guacamole_connection_id = guacamole.create_connection("Test 1", 'rdp', '3389', hostname, username, password, parent_identifier)
+        guacamole_connection_id = guacamole.create_connection("Test 1", 'rdp', '3389', hostname, "jin", "123456", "ROOT")
         guacamole_username = guacamole.create_user("Test 1", guacamole_password)
         guacamole.assign_connection(guacamole_username, guacamole_connection_id)
 
