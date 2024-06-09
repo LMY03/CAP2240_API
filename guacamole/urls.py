3feb7835d269f9940172a09a2f9fc8914902323f
LMY03 <86579817+LMY03@users.noreply.github.com>
2024-06-09 18:23:43 +0800

    a

--- guacamole/urls.py
+++ guacamole/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     path("delete_connection", views.delete_connection, name="delete_connection"),
     path("assign_connection", views.assign_connection, name="assign_connection"),
     path("revoke_connection", views.revoke_connection, name="revoke_connection"),
-    path("update_connection", views.revoke_connection, name="update_connection"),
+    path("update_connection", views.update_connection, name="update_connection"),
     path("get_connection_details", views.get_connection_details, name="get_connection_details"),
     path("get_connection_parameter_details", views.get_connection_parameter_details, name="get_connection_parameter_details"),
 ]
