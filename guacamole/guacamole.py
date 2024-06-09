50a22ee00f275fc4d3393f2503dbd479d9d72949
LMY03 <86579817+LMY03@users.noreply.github.com>
2024-06-09 18:49:52 +0800

    Update guacamole.py

--- guacamole/guacamole.py
+++ guacamole/guacamole.py
@@ -115,6 +115,7 @@ def get_connection_parameter_details(connection_id):
 
 # update connection 204
 def update_connection(connection_id, hostname):
+    print("updating")
     token = get_token()
     url = f"{GUACAMOLE_HOST}/guacamole/api/session/data/mysql/connections/{connection_id}?token={token}"
     headers = {'Content-Type': 'application/json'}
