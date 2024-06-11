6271d324b6a41d01ee3f1390be520675b97e505b
LMY03 <86579817+LMY03@users.noreply.github.com>
2024-06-11 22:23:37 +0800

    Update views.py

--- autotool/views.py
+++ autotool/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-import ansible_runner, os
+import ansible_runner
 from django.http import JsonResponse
 
 # Create your views here.
@@ -13,19 +13,12 @@ def run(request):
         return render(request, "data.html", { "data" : run_playbook() })
 
 def run_playbook():
-    playbook_path = '/playbooks/playbook.yml'  # Updated path
+    playbook_path = '/playbooks/playbook.yml'
     inventory_path = '/inventory/hosts'
-    private_data_dir = '/tmp/'
 
-    r = ansible_runner.run(private_data_dir=private_data_dir, playbook=playbook_path, inventory=inventory_path)
+    r = ansible_runner.run(private_data_dir='/tmp/', playbook=playbook_path, inventory=inventory_path)
 
     if r.status == 'successful':
         return JsonResponse({'status': 'success', 'data': r.get_fact_cache()})
     else:
-        stderr_path = os.path.join(private_data_dir, 'artifacts', r.rc, 'stderr')
-        try:
-            with open(stderr_path, 'r') as stderr_file:
-                stderr_output = stderr_file.read()
-        except Exception as e:
-            stderr_output = f"Error reading stderr file: {e}"
-        return JsonResponse({'status': 'failure', 'data': stderr_output})
+        return JsonResponse({'status': 'failure', 'data': r.stderr})
