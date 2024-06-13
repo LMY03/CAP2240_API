8e1ad241d9447ef7c00f264b49b7287fe99834e4
LMY03 <86579817+LMY03@users.noreply.github.com>
2024-06-13 12:36:45 +0800

    a

--- autotool/ansible.py
+++ autotool/ansible.py
@@ -1,2 +1,20 @@
+import subprocess
+import ansible_runner
+# ansible all -i /ansible/inventory/hosts -m ping -e 'ansible_ssh_common_args="-o StrictHostKeyChecking=no"'
 
-ANSIBLE_HOST = 'http://192.168.254.125'
+INVENTORY_HOSTS_PATH = '/ansible/inventory/hosts'
+
+def test_ping():
+    run_command("ansible all -i " + INVENTORY_HOSTS_PATH + "")
+
+def update_inventory_hosts(ip_add, vm_user):
+    with open(INVENTORY_HOSTS_PATH, 'w') as file:
+        file.write(ip_add + ' ansible_user=' + vm_user)
+    return "File has been edited successfully."
+
+def run_command(command): 
+    try:
+        result = subprocess.run(command, shell=True, check=True, stdout=subprocess.PIPE)
+        return result.stdout.decode()
+    except subprocess.CalledProcessError as e:
+        return str(e)
