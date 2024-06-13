31021a88137d5c30d01e84f90aa0476694be86fe
LMY03 <86579817+LMY03@users.noreply.github.com>
2024-06-13 10:07:56 +0800

    Update views.py

--- autotool/views.py
+++ autotool/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
-import ansible_runner, subprocess
+# import ansible_runner
+import subprocess
 from django.http import JsonResponse
 
 # Create your views here.
@@ -31,13 +32,13 @@ def run_command(command):
     except subprocess.CalledProcessError as e:
         return str(e)
 
-def run_playbook():
-    playbook_path = '/playbooks/playbook.yml'
-    inventory_path = '/inventory/hosts'
+# def run_playbook():
+#     playbook_path = '/playbooks/playbook.yml'
+#     inventory_path = '/inventory/hosts'
 
-    r = ansible_runner.run(private_data_dir='/tmp/', playbook=playbook_path, inventory=inventory_path)
+#     r = ansible_runner.run(private_data_dir='/tmp/', playbook=playbook_path, inventory=inventory_path)
 
-    if r.status == 'successful':
-        return JsonResponse({'status': 'success', 'data': r.get_fact_cache()})
-    else:
-        return JsonResponse({'status': 'failure', 'data': r.stderr})
+#     if r.status == 'successful':
+#         return JsonResponse({'status': 'success', 'data': r.get_fact_cache()})
+#     else:
+#         return JsonResponse({'status': 'failure', 'data': r.stderr})
