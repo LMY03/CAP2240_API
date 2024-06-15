023b535272df357852b1239ce0572cf1216336a4
LMY03 <86579817+LMY03@users.noreply.github.com>
2024-06-15 11:14:23 +0800

    Update ansible.py

--- autotool/ansible.py
+++ autotool/ansible.py
@@ -3,11 +3,13 @@ import subprocess
 # ansible all -i /ansible/inventory/hosts -m ping -e 'ansible_ssh_common_args="-o StrictHostKeyChecking=no"'
 
 INVENTORY_HOSTS_PATH = '/ansible/inventory/hosts'
+INVENTORY_HOSTS_PATH = '/ansible/inventory/hosts'
 
 def test_ping():
     run_command("ansible all -i " + INVENTORY_HOSTS_PATH + "")
 
 def update_inventory_hosts(ip_add, vm_user):
+    print("update inventory")
     with open(INVENTORY_HOSTS_PATH, 'w') as file:
         file.write(ip_add + ' ansible_user=' + vm_user)
     return "File has been edited successfully."
