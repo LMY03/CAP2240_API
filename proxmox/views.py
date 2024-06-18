15d43a6ed2366ecf47b0c0d5083dded238a2f9ec
LMY03 <86579817+LMY03@users.noreply.github.com>
2024-06-19 00:37:18 +0800

    a

--- proxmox/views.py
+++ proxmox/views.py
@@ -191,7 +191,7 @@ def status_lxc(request) :
         data = request.POST
         vmid = data.get("vmid")
 
-        status = proxmox.get_lxc_status(node, vmid)['lock']# [status]
+        status = proxmox.get_lxc_status(node, vmid)# [status]
 
         return render(request, "data.html", { "data" : status })
         
