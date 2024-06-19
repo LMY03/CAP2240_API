a8e3419357863bcbf44ab59dd80dce72d4a540c3
LMY03 <86579817+LMY03@users.noreply.github.com>
2024-06-19 22:32:31 +0800

    Update views.py

--- monitoring/views.py
+++ monitoring/views.py
@@ -3,4 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 
 def renders(request) : 
-    return render(request, "monitoring.html")
+    ip_add = {
+        "192.168.254.165",
+        "192.168.254.166",
+    }
+    return render(request, "monitoring.html", { "ip_add" : ip_add })
