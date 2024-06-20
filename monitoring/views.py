fe56cf04edb61125d7030932fddacebc7abd8a39
LMY03 <86579817+LMY03@users.noreply.github.com>
2024-06-20 23:56:47 +0800

    Update views.py

--- monitoring/views.py
+++ monitoring/views.py
@@ -12,6 +12,26 @@ import requests
 #     template = "index.html"
 #     return render(request, template, { "ip_add" : ip_add })
 
+# def fetch_netdata_metrics():
+#     base_url = "http://192.168.254.162:19999/api/v1/data?chart={chart}&format=json"
+#     charts = {
+#         'cpu': 'system.cpu',
+#         'memory': 'system.ram',
+#         'network': 'system.net'
+#     }
+#     data = {}
+#     for key, chart in charts.items():
+#         response = requests.get(base_url.format(chart=chart))
+#         if response.status_code == 200:
+#             data[key] = response.json()
+#         else:
+#             data[key] = {}
+#     return data
+
+# def renders(request):
+#     data = fetch_netdata_metrics()
+#     return render(request, 'index.html', {'data': data})
+
 def fetch_netdata_metrics():
     base_url = "http://192.168.254.162:19999/api/v1/data?chart={chart}&format=json"
     charts = {
@@ -23,11 +43,15 @@ def fetch_netdata_metrics():
     for key, chart in charts.items():
         response = requests.get(base_url.format(chart=chart))
         if response.status_code == 200:
-            data[key] = response.json()
+            chart_data = response.json()
+            data[key] = {
+                'labels': [label for label in chart_data['labels']],
+                'data': [point[0] for point in chart_data['data']]
+            }
         else:
-            data[key] = {}
+            data[key] = {'labels': [], 'data': []}
     return data
 
-def renders(request):
+def netdata_view(request):
     data = fetch_netdata_metrics()
-    return render(request, 'index.html', {'data': data})
+    return render(request, 'netdata.html', {'data': data})
