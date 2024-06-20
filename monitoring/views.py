760b71e1c2ae843117007c6ac8958321f6bec165
LMY03 <86579817+LMY03@users.noreply.github.com>
2024-06-20 23:20:36 +0800

    Update views.py

--- monitoring/views.py
+++ monitoring/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+import requests
 
 # Create your views here.
 
@@ -8,3 +9,23 @@ def renders(request) :
         "192.168.254.166",
     }
     return render(request, "monitoring.html", { "ip_add" : ip_add })
+
+def fetch_netdata_metrics():
+    base_url = "http://192.168.254.162:19999/api/v1/data?chart={chart}&format=json"
+    charts = {
+        'cpu': 'system.cpu',
+        'memory': 'system.ram',
+        'network': 'system.net'
+    }
+    data = {}
+    for key, chart in charts.items():
+        response = requests.get(base_url.format(chart=chart))
+        if response.status_code == 200:
+            data[key] = response.json()
+        else:
+            data[key] = {}
+    return data
+
+def netdata_view(request):
+    data = fetch_netdata_metrics()
+    return render(request, 'netdata.html', {'data': data})
