388ad82d442b2d084de53ca5a3091cd4a0b3bddd
LMY03 <86579817+LMY03@users.noreply.github.com>
2024-07-09 20:07:45 +0800

    Update pfsense.py

--- pfsense/pfsense.py
+++ pfsense/pfsense.py
@@ -72,7 +72,7 @@ def delete_firewall_rule(id):
         'Content-Type': 'application/json',
         'Authorization': f"Bearer {token}",
     }
-    print("url")
+    print(url)
     response = requests.delete(url, headers=headers)
     return response.json()
 
