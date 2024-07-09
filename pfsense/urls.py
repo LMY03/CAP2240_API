cf1cf2087e3f1c17e686b961503ea8133a68a36e
LMY03 <86579817+LMY03@users.noreply.github.com>
2024-07-10 01:47:22 +0800

    A

--- pfsense/urls.py
+++ pfsense/urls.py
@@ -6,9 +6,9 @@ app_name = "pfsense"
 
 urlpatterns = [
     path('', views.renders, name='index'),
-    path('add_firewall_rule', views.add_firewall_rule, name='add_firewall_rule'),
-    path('edit_firewall_rule', views.edit_firewall_rule, name='edit_firewall_rule'),
-    path('delete_firewall_rule', views.delete_firewall_rule, name='delete_firewall_rule'),
+    path('add_port_forward_rule', views.add_port_forward_rule, name='add_port_forward_rule'),
+    path('edit_port_forward_rule', views.edit_port_forward_rule, name='edit_port_forward_rule'),
+    path('delete_port_forward_rule', views.delete_port_forward_rule, name='delete_port_forward_rule'),
     path('get_port_forward_rules', views.get_port_forward_rules, name='get_port_forward_rules'),
     path('get_firewall_rules', views.get_firewall_rules, name='get_firewall_rules'),
 ]
