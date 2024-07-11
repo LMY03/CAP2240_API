a0e4dd0ab72be793518ef8215eddf91ce39d9831
LMY03 <86579817+LMY03@users.noreply.github.com>
2024-07-11 21:42:14 +0800

    Update views.py

--- pfsense/views.py
+++ pfsense/views.py
@@ -62,7 +62,8 @@ def add_port_forward_rules(request_id):
     pfsense.apply_changes()
 
 def update_port_forward_rule(vm_name, ip_add):
-    pfsense.edit_firewall_rule(get_firewall_rule(vm_name), ip_add)
+    print("---------------------------------")
+    print(pfsense.edit_firewall_rule(get_firewall_rule(vm_name), ip_add))
     pfsense.edit_port_forward_rule(get_port_forward_rule(vm_name), ip_add)
     pfsense.apply_changes()
 
