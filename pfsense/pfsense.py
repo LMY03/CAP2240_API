24af642edef9d3394a7be6907d40cb44810d51af
LMY03 <86579817+LMY03@users.noreply.github.com>
2024-07-11 19:45:14 +0800

    Update pfsense.py

--- pfsense/pfsense.py
+++ pfsense/pfsense.py
@@ -42,7 +42,7 @@ def add_port_forward_rule(protocol, destination_port, ip_add, local_port, descr)
         # 'nordr': True, # notsure
         # 'nosync': True,
         'descr': descr,
-        'associated_rule_id': '',
+        # 'associated_rule_id': '',
     }
     response = requests.post(url, headers=headers, json=data)
     print(response.json())
