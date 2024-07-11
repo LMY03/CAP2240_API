0c2bf6aac5ff1d206df54e4e8cdceec0e2bf396f
LMY03 <86579817+LMY03@users.noreply.github.com>
2024-07-11 19:36:35 +0800

    Update pfsense.py

--- pfsense/pfsense.py
+++ pfsense/pfsense.py
@@ -88,9 +88,7 @@ def add_firewall_rule(protocol, destination_port, ip_add, descr):
         'descr': descr,
     }
     response = requests.post(url, headers=headers, json=data)
-    print(response.json)
-    print(response.json())
-    return response.json()
+    return response.json()['data']['id']
 
 def edit_firewall_rule(rule_id, ip_add):
     token = get_token()
