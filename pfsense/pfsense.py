c185394852a983f67f47f6aae434c93d32a00b63
LMY03 <86579817+LMY03@users.noreply.github.com>
2024-07-11 22:33:24 +0800

    Update pfsense.py

--- pfsense/pfsense.py
+++ pfsense/pfsense.py
@@ -5,9 +5,10 @@ PFSENSE_HOST = 'http://192.168.1.1'
 API_KEY = 'de6b87c7d2afb198244c69fbdbe7fbbe'
 
 def get_token():
-    url = f'{PFSENSE_HOST}/api/v2/auth/jwt'
-    response = requests.post(url, auth=HTTPBasicAuth("admin", "pfsense"))
-    return response.json()['data']['token']
+    return API_KEY
+    # url = f'{PFSENSE_HOST}/api/v2/auth/jwt'
+    # response = requests.post(url, auth=HTTPBasicAuth("admin", "pfsense"))
+    # return response.json()['data']['token']
 
 def apply_changes():
     token = get_token()
