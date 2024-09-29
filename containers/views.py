c5d94791486c56cb3b6a0ce8fcaa360dad71cc2d
LMY03 <86579817+LMY03@users.noreply.github.com>
2024-09-29 17:40:27 +0800

    /

--- containers/views.py
+++ containers/views.py
@@ -1,8 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-from .proxmox import clone_lxc
+from . import proxmox
 
 # Create your views here.
 
 def renders(request) : 
     return render(request, "containers/form.html")
+
+def clone_lxc(request):
+
+    if request.method == "POST":
+
+        data = request.POST
+        vm_id = data.get("vm_id")
+        newid = data.get("newid")
+
+        data = proxmox.clone_lxc("jin", vm_id, newid, newid)
+
+        return render(request, "containers/data.html", { 'data' : data })
+    
+    return redirect('containers:form')
