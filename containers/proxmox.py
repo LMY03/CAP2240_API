eed68b2317edc9193c8623e07587244c5634dd09
LMY03 <86579817+LMY03@users.noreply.github.com>
2024-09-30 15:32:52 +0800

    Update proxmox.py

--- containers/proxmox.py
+++ containers/proxmox.py
@@ -156,7 +156,7 @@ def check_clone_status(node, vm_id):
     return True
 
 def start_lxc(node, vm_id):
-    a = get_proxmox_client().nodes(node).lxc(vm_id).status.start()
+    a = get_proxmox_client().nodes(node).lxc(vm_id).status.start().post()
     print(a)
 
 
