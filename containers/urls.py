455a16d221e2282e935d6ea4d4e5fcd42a0aeec0
LMY03 <86579817+LMY03@users.noreply.github.com>
2024-09-30 15:29:01 +0800

    Update urls.py

--- containers/urls.py
+++ containers/urls.py
@@ -7,4 +7,5 @@ app_name = "containers"
 urlpatterns = [
     path("", views.renders, name="form"),
     path("clone_lxc", views.clone_lxc, name="clone_lxc"),
+    path("start_lxc", views.start_lxc, name="start_lxc"),
 ]
